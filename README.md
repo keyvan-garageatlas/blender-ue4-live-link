b2164742a351aea0f209c663777f6bfcbb2fd6be
Tom Delaney <delaneyt1@gmail.com>
2019-10-27 10:07:16 -0400

    Add test panel with test operator

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # blender-ue4-live-link  
+[![Build Status](https://travis-ci.org/tscrypter/blender-ue4-live-link.svg?branch=master)](https://travis-ci.org/tscrypter/blender-ue4-live-link)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=tscrypter_blender-ue4-live-link&metric=alert_status)](https://sonarcloud.io/dashboard?id=tscrypter_blender-ue4-live-link)  
 Blender (2.8) plugin to provide Live Link integration with Unreal Engine 4
--- __init__.py
+++ __init__.py
@@ -10,6 +10,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+import bpy
+
 
 bl_info = {
     "name": "Unreal Engine 4 Live Link",
@@ -23,9 +25,30 @@ bl_info = {
 }
 
 
-def register():
-    ...
+class Test_OT_Operator(bpy.types.Operator):
+    bl_idname = "view3d.cursor_center"
+    bl_label = "Simple operator"
+    bl_description = "Center 3d cursor"
+
+    def execute(self, context):
+        bpy.ops.view3d.snap_cursor_to_center()
+        return {'FINISHED'}
 
 
-def unregister():
-    ...
+class Test_PT_Panel(bpy.types.Panel):
+    bl_idname = "Test_PT_Panel"
+    bl_label = "Test Panel"
+    bl_category = "Test Addon"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    def draw(self, context):
+        layout = self.layout
+
+        row = layout.row()
+        row.operator('view3d.cursor_center', text="Center 3D cursor")
+
+
+classes = (Test_OT_Operator, Test_PT_Panel)
+
+register, unregister = bpy.utils.register_classes_factory(classes)
